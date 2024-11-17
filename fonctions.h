febae35e2dae83136fbb0af781662ce80a0b5885
H-Boit <boitet_hugo@hotmail.com>
2024-11-17 19:21:37 +0100

    Update fonctions.h

--- fonctions.h
+++ fonctions.h
@@ -7,6 +7,6 @@
 void Init_tab(int plateau[Lenght_tab][Lenght_tab]);
 void Print_tab(int plateau[Lenght_tab][Lenght_tab]); 
 bool Is_empty(int plateau[Lenght_tab][Lenght_tab], int ligne, int column); 
-bool Is_possible(int plateau[Lenght_tab][Lenght_tab], int i, int j, int Indice_player);
+bool Is_possible(int plateau[Lenght_tab][Lenght_tab], int i, int j, int Indice_player, int Possible_vect[8][2]);
 
 // Fonctions de IA.c
